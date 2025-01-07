1a61deeeb7c0508355bbc14605671a399feafcc8
Nicola Dal Molin <nicola.dalmolin.24@stud.itsaltoadriatico.it>
2025-01-07 12:11:58 +0100

    Struttura base esercizio fattoriale

---
+++ calcolo_fattoriale.py
@@ -0,0 +1,8 @@
+""" Esercizio per esercitarsi usando i comandi di Git
+Tale esercizio permette di calcolare il fattoriale da una variabile """
+
+fattore = 1
+
+def calcolo_fattoriale(fattore=1): #di default è 1 se non gli viene passato niente
+    return 0
+    
