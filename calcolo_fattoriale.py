2fb6c176c8b86b4d652877862d751dcd35b510e8
Nicola Dal Molin <nicola.dalmolin.24@stud.itsaltoadriatico.it>
2025-01-07 12:21:11 +0100

    conclusione_progetto

--- calcolo_fattoriale.py
+++ calcolo_fattoriale.py
@@ -1,12 +1,14 @@
 """ Esercizio per esercitarsi usando i comandi di Git
 Tale esercizio permette di calcolare il fattoriale da una variabile """
 
-fattore = 1
-
 def calcolo_fattoriale(fattore=0): #di default è 1 se non gli viene passato niente
     if(fattore == 0):
         return 1
-    result = 0
-    for i in range(1, fattore):
-        
+    result = 1
+    for i in range(1, fattore+1):
+        result = result * i
+    return result
+
+fattore = 4
+print(calcolo_fattoriale(fattore))
     
