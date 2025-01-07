815df6bb4f4962b4228da1d84fbb2fbb02c0a3fd
Nicola Dal Molin <nicola.dalmolin.24@stud.itsaltoadriatico.it>
2025-01-07 12:15:52 +0100

    modifica e aggiunta ciclo for

--- calcolo_fattoriale.py
+++ calcolo_fattoriale.py
@@ -3,6 +3,10 @@ Tale esercizio permette di calcolare il fattoriale da una variabile """
 
 fattore = 1
 
-def calcolo_fattoriale(fattore=1): #di default è 1 se non gli viene passato niente
-    return 0
+def calcolo_fattoriale(fattore=0): #di default è 1 se non gli viene passato niente
+    if(fattore == 0):
+        return 1
+    result = 0
+    for i in range(1, fattore):
+        
     
