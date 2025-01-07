d7f31c26dfa4fe8859a05098dd40b6d17abe8d16
Nicola Dal Molin <nicola.dalmolin.24@stud.itsaltoadriatico.it>
2025-01-07 12:23:14 +0100

    aggiunto commento

--- calcolo_fattoriale.py
+++ calcolo_fattoriale.py
@@ -5,7 +5,7 @@ def calcolo_fattoriale(fattore=0): #di default è 1 se non gli viene passato nie
     if(fattore == 0):
         return 1
     result = 1
-    for i in range(1, fattore+1):
+    for i in range(1, fattore+1): #attenzione che il range eslude l'estremo "destro"
         result = result * i
     return result
 
