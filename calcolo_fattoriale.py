2c806e72f5ed4d42ba3dd9c2d8408b6e110a5f4c
Nicola Dal Molin <nicola.dalmolin.24@stud.itsaltoadriatico.it>
2025-01-07 12:51:51 +0100

    modifica e uso della ricorsione

--- calcolo_fattoriale.py
+++ calcolo_fattoriale.py
@@ -1,14 +1,14 @@
 """ Esercizio per esercitarsi usando i comandi di Git
 Tale esercizio permette di calcolare il fattoriale da una variabile """
-
-def calcolo_fattoriale(fattore=0): #di default è 1 se non gli viene passato niente
-    if(fattore == 0):
-        return 1
-    result = 1
-    for i in range(1, fattore+1): #attenzione che il range eslude l'estremo "destro"
-        result = result * i
-    return result
+def calcolo_fattoriale_ricorsivo(fattore, ini=1):
+    # Caso base: se il fattore è 0 o 1, ritorna il risultato accumulato
+    if fattore == 0 or fattore == 1:
+        return ini
+    # Chiamata ricorsiva con fattore decrementato e risultato aggiornato
+    return calcolo_fattoriale_ricorsivo(fattore - 1, ini * fattore)
 
 fattore = 4
-print(calcolo_fattoriale(fattore))
+start = 1
+print(calcolo_fattoriale_ricorsivo(fattore, start))
+
     
